42e18ecce79e746f494c69cd5b2deafb3265d73f
unknown <codyt11@hotmail.com>
2023-11-11 23:06:39 -0600

    started display page for crownclothing

--- codytownley/src/Pages/CrownClothing.js
+++ codytownley/src/Pages/CrownClothing.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./crownClothing.scss";
+import crown from "../images/crwn.mp4";
 
 function Crwn() {
     return(
     <div className="crown-main">
-        Crown Clothing!
+        <div className="display-top">
+            <div className="project-video">
+                <video autoPlay loop muted>
+                    <source src={crown} type="video/mp4" />
+                </video>
+                <div className="title">Crown Clothing</div>
+                <p className="short-about">E-Commerce Clothing Store</p>
+                <div className="roles">
+                    <div className="my-role"><span className="first-word">ROLE</span> FULL STACK DEVELOPER</div>
+                    <div className="context"><span className="first-word">CONTEXT</span> TEST BUILD</div>
+                    <div className="period"><span className="first-word">PERIOD</span> FEBRUARY 2023</div>
+                </div>
+            </div>
+            <div className="project-about">
+                <div project-title>The Project</div>
+                <p className="project-about">sadflkjsad;flasdf asfdl;kjasdfl;kjas fas;lkdfja;lskjdf as;lkdfjla;skjfd asldkfj asl;kdjfl;aksjfd a;slkdjf;alskjdf ;lkasjdflkasjdf</p>
+            </div>
+        </div>
+        <div className="display-bottom">bottom</div>
     </div>
 )};
 
